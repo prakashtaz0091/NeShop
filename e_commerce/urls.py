782730b67364b3b49f53d8c491b3bed8339a2149
prakashtaz0091 <code.ing2468@gmail.com>
2025-01-23 20:30:08 +0545

    base setup

---
+++ e_commerce/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    #account
+    path('account/', include('account.urls')),
+    
+    # #shop
+    # path('', include('shop.urls')),
+    
+    # #cart
+    # path('cart/', include('cart.urls')),
+    
+    # #customer
+    # path('customer/', include('customer.urls')),
+    
+    # #vendor
+    # path('vendor/', include('vendor.urls')),
+]
